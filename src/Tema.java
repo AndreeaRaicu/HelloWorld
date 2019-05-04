7a58d2f45114863d0bf19da817ceef3153ac9dfa
AndreeaRaicu <andreea.raicu22@yahoo.com>
2019-05-04 20:04:46 +0300

    exercitiu 1

--- src/Tema.java
+++ src/Tema.java
@@ -1,29 +1,11 @@
 public class Tema {
 
-   2.
+    public static void main(String[] args){
 
-    public class Main{
-        public static void main(String[] args){
-
-            int n = 6;
-            int counter=1;
-            System.out.println("N=" +n);
-            do {
-                if (n % 2 == 0) {
-                    n = n / 2;
-                } else {
-                    n = 3 * n + 1;
-                }
-                System.out.println("N=" +n);
-                counter++;
-
-            } while (n!=1);
-
-            System.out.println(counter);
 
         }
 
     }
 
 
-}
+
