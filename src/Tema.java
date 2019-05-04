ba320324e42394dae4445bf8d783512f052265c6
AndreeaRaicu <andreea.raicu22@yahoo.com>
2019-05-04 19:44:38 +0300

    exercitul 2

---
+++ src/Tema.java
@@ -0,0 +1,30 @@
+public class Tema {
+
+    public static void main(String[] args) {
+
+
+        // 1. sa se scrie un program folosind o structura repetitiva, care sa afiseze urmatorul rezultat
+//# ##  ###   ####   ###  ##  #
+
+
+        int n = 4;
+        int k = 1;
+        boolean crestere = true;
+
+        while (k > 0) {
+            for (int i = 0; i < k; i++)
+                System.out.print("#");
+
+            System.out.print(" ");
+            if (crestere == true) {
+                if (k < n) {
+                    k++;
+                } else {
+                    k--;
+                    crestere = false;
+                }
+            } else
+                k--;
+        }
+    }
+}
