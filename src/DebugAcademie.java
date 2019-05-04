7e89060c595843156fe6237962c3907c8b10235a
AndreeaRaicu <andreea.raicu22@yahoo.com>
2019-05-04 19:17:14 +0300

    exercitiul 1

---
+++ src/DebugAcademie.java
@@ -0,0 +1,5 @@
+public class DebugAcademie {
+    public static void main(String[] args){
+System.out.println("cifra");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
+1.
+
 public class Main {
 
+
     public static void main(String[] args) {
-        System.out.println("Hello World TEMA2!");
+
+
+        // 1. sa se scrie un program folosind o structura repetitiva, care sa afiseze urmatorul rezultat
+//# ##  ###   ####   ###  ##  #
+
+
+        int n = 4;
+        int k = 1;
+        boolean crestere = true;
+
+        while (k > 0) {
+            for (int i = 0; i < k; i++)
+                System.out.print("#");
+
+            System.out.print(" ");
+            if (crestere == true) {
+                if (k < n) {
+                    k++;
+                } else {
+                    k--;
+                    crestere = false;
+                }
+            } else
+                k--;
+        }
     }
 }
+
+
