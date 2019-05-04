6e63faf2a24284c4d978ec8f71fcb82839ad7bc0
AndreeaRaicu <andreea.raicu22@yahoo.com>
2019-05-04 23:37:30 +0300

    ex

--- src/Bubblesort.java
+++ src/Bubblesort.java
@@ -1,7 +1,18 @@
+//Scrieti un program care sa  
+// a.  sorteze un array de numere naturale. (ex {8, 4, 9,1, 2, 222})
+//        - folosind bubble sort si java
+
+//        b.  dupa ce s-a efectuat sortarea verificati daca exista in acea lista sortata
+//        deja un numar n=? - folosind binary search si java 
+
+
+
+
+
 public class Bubblesort {
     public static void main(String args[] ) {
 
-        int[] myArray=new int [] {100git add git,4,8,1,2,222};
+        int[] myArray=new int [] {100,4,8,1,2,222};
         int n=6;
         //int arr[]; int n = arr.length;  echivalent cu: int[] myArray=new int [] {1,23,4}; int n=6; ???????
         for (int i = 0; i < n-1; i++)
